0352918b204d27bf61c9a7282f2fab4c9c533b2b
eraj2587 <e.raj2587@gmail.com>
2021-02-27 23:50:21 +0530

    changes in counter

--- src/component/counter.jsx
+++ src/component/counter.jsx
@@ -1,13 +1,28 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 1,
+  };
+
   render() {
     return (
       <React.Fragment>
-        <h1>Hello World</h1>
-        <button>Increment</button>
+        <h1 className={this.getClasses()}>{this.formatCount()}</h1>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
+  }
+
+  getClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? <h1>Zero</h1> : count;
   }
 }
 
